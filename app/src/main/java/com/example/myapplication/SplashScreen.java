f8e72dfffef7a01424c8240edccd069f30572d19
Ghulam Hussain Joyo <ghulamhussain.kcsf18@iba-suk.edu.pk>
2021-11-17 07:30:02 +0500

    Lab 1 added

--- app/src/main/java/com/example/myapplication/SplashScreen.java
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.lab_4.act2;
 import com.example.myapplication.lab_5.Cookies;
 import com.example.myapplication.lab_6.Login;
+import com.example.myapplication.lab_7.mp3Player;
 
 import static com.example.myapplication.R.layout.activity_splash_screen;
 
@@ -22,7 +23,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(), Assignment_1.class));
+                startActivity(new Intent(getApplicationContext(), mp3Player.class));
             }
         },3000);
     }
